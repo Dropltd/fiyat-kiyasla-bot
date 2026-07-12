16d10704aef2bc02472f2ed3a91c31326803cc15
Dropltd <sazan7734@gmail.com>
2026-07-13 02:19:40 +0300

    Update bim.py

--- scraper/bim.py
+++ scraper/bim.py
@@ -1,6 +1,4 @@
-import json
 import requests
-
 
 URL = "https://bim.veesk.net/service/v1.0/home/aktuel"
 
@@ -15,18 +13,39 @@ def run():
         URL,
         headers=HEADERS,
         json={"token": ""},
-        timeout=30,
+        timeout=30
     )
 
-    print("Status Code:", response.status_code)
-
-    if response.status_code != 200:
-        print(response.text)
-        return
+    response.raise_for_status()
 
     data = response.json()
 
-    with open("output.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+    products = []
 
-    print("output.json oluşturuldu.")
+    for section in data["widgets"]["aktuel"]:
+        section_name = section["title"]
+
+        for category in section["childs"]:
+            category_name = category["title"]
+
+            for product in category["products"]:
+
+                products.append({
+                    "sku": product.get("sku"),
+                    "name": product.get("title"),
+                    "brand": product.get("brands_label"),
+                    "category": category_name,
+                    "section": section_name,
+                    "price": product.get("price"),
+                    "special_price": product.get("special_price"),
+                    "discount": product.get("discount_percent"),
+                    "image": product.get("photo"),
+                    "store": "BİM"
+                })
+
+    print(f"Toplam {len(products)} ürün bulundu.")
+
+    for product in products[:10]:
+        print(product)
+
+    return products
