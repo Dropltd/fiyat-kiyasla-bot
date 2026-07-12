7f543f33f7ba943c9ec0d84566038bc5c5b86f12
Dropltd <sazan7734@gmail.com>
2026-07-13 02:55:10 +0300

    Update firestore_service.py

--- services/firestore_service.py
+++ services/firestore_service.py
@@ -1,4 +1,13 @@
-from firebase_admin import firestore
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(
+        json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    )
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
@@ -10,9 +19,7 @@ def save_products(products):
 
     for product in products:
         sku = product["sku"]
-
         collection.document(str(sku)).set(product)
-
         count += 1
 
     print(f"{count} ürün Firestore'a kaydedildi.")
