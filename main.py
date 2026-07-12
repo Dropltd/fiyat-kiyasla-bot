45d227026282d46cef94f964ea1e98c0d335385d
Dropltd <sazan7734@gmail.com>
2026-07-13 00:13:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 """
 Fiyat Kıyasla Bot
-Main Entry Point
+Main
 """
 
-from firebase import get_firestore
+from scraper.bim import BimScraper
+from services.firestore_service import save_product
 from utils.logger import get_logger
 
-logger = get_logger("MarketBot")
+logger = get_logger("MAIN")
+
+
+def run_bim():
+
+    logger.info("BİM taraması başlıyor...")
+
+    scraper = BimScraper()
+
+    products = scraper.run()
+
+    saved = 0
+
+    for product in products:
+
+        if save_product(product):
+            saved += 1
+
+    logger.info(f"Kaydedilen ürün sayısı : {saved}")
 
 
 def main():
-    logger.info("Bot başlatıldı.")
 
-    db = get_firestore()
+    logger.info("Bot çalışmaya başladı.")
 
-    logger.info("Firestore bağlantısı başarılı.")
+    run_bim()
 
-    logger.info("Şimdilik test çalışması tamamlandı.")
+    logger.info("Bot tamamlandı.")
 
 
 if __name__ == "__main__":
