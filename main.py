720241e9f4d59c792089df6125ac19f4304ca484
Dropltd <sazan7734@gmail.com>
2026-07-13 02:15:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import requests
-import json
+from scraper.bim import run
 
-url = "https://bim.veesk.net/service/v1.0/home/aktuel"
 
-headers = {
-    "Content-Type": "application/json",
-    "X-App-version": "20241101"
-}
+def main():
+    print("Fiyat Kıyasla Bot Başladı")
+    run()
+    print("Bot Tamamlandı")
 
-response = requests.post(
-    url,
-    headers=headers,
-    json={"token": ""}
-)
 
-data = response.json()
-
-with open("output.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
-
-print("JSON kaydedildi.")
+if __name__ == "__main__":
+    main()
